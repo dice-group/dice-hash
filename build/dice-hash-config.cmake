176e6d63c3d9d3bfd1fd1e4cb7733d1dca0e2706
Lukas Kerkemeier <kerkemei@campus.uni-paderborn.de>
2021-02-11 16:17:23 +0100

    removed unwanted folders (added by accident).

--- build/dice-hash-config.cmake
+++
@@ -1,5 +0,0 @@
-# Dummy config file
-# When a dependency is added with add_subdirectory, but searched with find_package
-
-# Redirect to the directory added with add_subdirectory
-add_subdirectory(/home/lukas/Dokumente/dice_arbeit/dice-hash /home/lukas/Dokumente/dice_arbeit/dice-hash/build)
