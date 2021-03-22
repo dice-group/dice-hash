55cdd408d05caafbdad505260b0da916b87cda1f
bigerl <bigerl@users.noreply.github.com>
2021-03-22 08:34:30 +0000

    deploy: 54f40e203ef722c7e053a4770c601a021adade3e

--- search/pages_0.js
+++ search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['dice_2dhash_3a_20high_2dperformance_20hash_20for_20hash_2dbased_20container_77',['dice-hash: High-Performance Hash for Hash-Based Container',['../index.html',1,'']]]
+  ['dice_2dhash_3a_20a_20hashing_20framework_77',['dice-hash: A Hashing framework',['../index.html',1,'']]]
 ];
