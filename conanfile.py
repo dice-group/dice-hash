f8332cbd0798517cc7034d492c5ca463f92aa367
Liss Heidrich <Clueliss@users.noreply.github.com>
2023-11-13 13:52:21 +0100

    add cmake stuff

--- conanfile.py
+++ conanfile.py
@@ -46,3 +46,7 @@ class DiceHashConan(ConanFile):
     def package_info(self):
         self.cpp_info.bindirs = []
         self.cpp_info.libdirs = []
+
+        self.cpp_info.set_property("cmake_find_mode", "both")
+        self.cpp_info.set_property("cmake_target_name", "dice-hash::dice-hash")
+        self.cpp_info.set_property("cmake_file_name", "dice-hash")
