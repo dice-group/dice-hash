e87c8e57820f89aa301b4d2449f2beb523ea505f
Liss Heidrich <31625940+Clueliss@users.noreply.github.com>
2024-07-22 13:57:43 +0200

    add pobr diff

--- include/dice/hash.hpp
+++ include/dice/hash.hpp
@@ -1,4 +1,5 @@
 #ifndef DICE_HASH_HASH_HPP
 #define DICE_HASH_HASH_HPP
-#include "dice/hash/DiceHash.hpp"
+#include <dice/hash/DiceHash.hpp>
+#include <dice/hash/version.hpp>
 #endif//DICE_HASH_HASH_HPP
