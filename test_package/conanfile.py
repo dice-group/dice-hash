4e0c6337a40c9ff84c9122c231eda65e67aefc92
mcb <mgrbo@web.de>
2024-08-05 14:32:47 +0200

    fix test_package

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -9,7 +9,7 @@ required_conan_version = ">=1.59"
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps", "CMakeToolchain"
-    default_options = {"dice-hash:with_sodium": True}
+    default_options = {"dice-hash/*:with_sodium": True}
 
     def requirements(self):
         self.requires(self.tested_reference_str)
