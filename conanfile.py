dea9b355cad7bff6b965d9e15bfeb3f9c50eac53
Liss Heidrich <31625940+liss-h@users.noreply.github.com>
2024-11-18 15:06:08 +0100

    rename files

--- conanfile.py
+++ conanfile.py
@@ -26,8 +26,8 @@ class DiceHashConan(ConanFile):
         "with_test_deps": False,
         "with_sodium": False
     }
-    exports = "LICENSE", "LICENSE_A"
-    exports_sources = "include/*", "CMakeLists.txt", "cmake/*", "LICENSE", "LICENSE_A"
+    exports = "LICENSE-MIT", "LICENSE-APACHE"
+    exports_sources = "include/*", "CMakeLists.txt", "cmake/*", "LICENSE-MIT", "LICENSE-APACHE"
 
     generators = ("CMakeDeps", "CMakeToolchain")
 
